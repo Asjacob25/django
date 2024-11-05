fb6f740e5e387c6f0c32784625080f8ff41d0be9
Ashley Jacob <128179385+Asjacob25@users.noreply.github.com>
2024-11-05 13:10:09 -0500

    hello

--- django/forms/boundfield.py
+++ django/forms/boundfield.py
@@ -6,7 +6,7 @@ from django.forms.widgets import MultiWidget, Textarea, TextInput
 from django.utils.functional import cached_property
 from django.utils.html import format_html, html_safe
 from django.utils.translation import gettext_lazy as _
-#test
+#testt
 __all__ = ("BoundField",)
 
 
